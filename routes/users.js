9cb0b9590af1a25e58d7fea62b799b6a7c578d49
Agba Emmanuel <agbaayoh@gmail.com>
2022-02-09 05:35:16 -0700

    mongoDb database connected sucessfully

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
@@ -17,13 +18,39 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed')
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email })
+
+      if (user) {
+        return res.status(400).json({msg: 'User already exists'})
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      })
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User saved');
+
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
