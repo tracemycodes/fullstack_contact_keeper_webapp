2155dbfc4a097850388531719b228e0ae60491a2
Agba Emmanuel <agbaayoh@gmail.com>
2022-02-16 06:09:18 -0700

    json web token authentification

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
-const bcrypt = require('bcryptjs/dist/bcrypt');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -45,7 +47,20 @@ router.post(
 
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(payload, config.get('jwtSecret'), { 
+        expiresIn: 360000
+      }, (err, token) => {
+        if (err) {
+          throw err;
+        }
+        res.json({ token });
+      });
 
     } catch (error) {
       console.log(error.message);
