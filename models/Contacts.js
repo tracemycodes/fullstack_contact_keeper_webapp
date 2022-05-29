7cb9be4776bccc40613a1880b4d89743fab76651
Agba Emmanuel <agbaayoh@gmail.com>
2022-05-29 02:53:45 -0700

    feat: contact schema model established

---
+++ models/Contacts.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+const ContactsSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }  
+})
+
+module.exports = mongoose.model('user', ContactsSchema)
