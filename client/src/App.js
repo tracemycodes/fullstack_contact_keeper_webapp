2cd054dc33e91c0abca05a69be6fcedfd85df5a6
Agba Emmanuel <agbaayoh@gmail.com>
2022-05-30 04:33:54 -0700

    component routing implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layouts/Navbar';
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import { Fragment } from 'react';
 
 const App = () => {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <div className="container">
+          <Routes>
+            <Route exact path='/' element={<Home/>} />
+            <Route exact path='/about' element={<About/>} />
+          </Routes>
+        </div>
+      </Fragment>
+    </Router>
+    
   );
 }
 
