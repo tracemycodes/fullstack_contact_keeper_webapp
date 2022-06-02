03a138a0e451d592193bc09fb254a6e1d866211b
Agba Emmanuel <agbaayoh@gmail.com>
2022-06-02 03:57:50 -0700

    Loading component and state established

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -2,9 +2,11 @@ import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/contactContext';
 
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
 
   const { isAuthenticated, logoutUser, user } = authContext;
 
