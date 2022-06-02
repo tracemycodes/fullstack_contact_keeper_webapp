d22f822fd2646c6a1c83228ee9e837e588b4bad3
Agba Emmanuel <agbaayoh@gmail.com>
2022-06-02 07:53:39 -0700

    static assets for probuction build

--- server.js
+++ server.js
@@ -6,20 +6,24 @@ const app = express();
 // Connect Database
 connectDB();
 
-
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-
 // Define our routes
-app.use('/api/users', require(`./routes/users`))
-app.use('/api/auth', require(`./routes/auth`))
-app.use('/api/contacts', require(`./routes/contacts`))
+app.use('/api/users', require(`./routes/users`));
+app.use('/api/auth', require(`./routes/auth`));
+app.use('/api/contacts', require(`./routes/contacts`));
 
-const PORT = process.env.PORT || 5000
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
